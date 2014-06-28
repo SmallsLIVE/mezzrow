2d44b6d707b7b25327d618f30363e569591b1ad0
Filip Jukic <filip@jukic.me>
2014-06-28 14:11:30 +0200

    serve media files using dj_static

--- mezzrow/mezzrow/wsgi.py
+++ mezzrow/mezzrow/wsgi.py
@@ -11,5 +11,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mezzrow.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+from dj_static import Cling, MediaCling
+application = Cling(MediaCling(get_wsgi_application()))
