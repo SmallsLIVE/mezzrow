3a210baee708b5ed08d13d4a0401ac03d91e76a8
Filip Jukic <filip@jukic.me>
2014-07-01 11:27:13 +0200

    caching enabled for production

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python mezzrow/manage.py syncdb --noinput; python mezzrow/manage.py migrate --merge --noinput; newrelic-admin run-program gunicorn --pythonpath mezzrow mezzrow.wsgi --workers $WEB_CONCURRENCY 
+web: python mezzrow/manage.py syncdb --noinput; python mezzrow/manage.py migrate --merge --noinput; newrelic-admin run-program gunicorn --pythonpath mezzrow mezzrow.wsgi --workers $WEB_CONCURRENCY
--- mezzrow/mezzrow/settings/heroku.py
+++ mezzrow/mezzrow/settings/heroku.py
@@ -1,6 +1,6 @@
 import os
-from .base import *
 import dj_database_url
+from .base import *
 
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "herokudefault")
@@ -14,6 +14,33 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+# Memcached
+CACHES = {
+    'default': {
+        'BACKEND': 'django_bmemcached.memcached.BMemcached',
+        'LOCATION': os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
+        'OPTIONS': {
+            'username': os.environ.get('MEMCACHEDCLOUD_USERNAME'),
+            'password': os.environ.get('MEMCACHEDCLOUD_PASSWORD')
+        }
+    }
+}
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
+) + MIDDLEWARE_CLASSES + (
+    'django.middleware.cache.FetchFromCacheMiddleware',
+)
+
+CACHE_MIDDLEWARE_SECONDS = 300
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
 
 # Static asset configuration
 import os
