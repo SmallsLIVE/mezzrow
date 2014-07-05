629292d19ccdc62e00be6cf3f01d1fccfa897911
eune <luke.aune@gmail.com>
2014-07-05 14:44:58 -0500

    added DB settings

--- mezzrow/mezzrow/settings/local_luke.py
+++ mezzrow/mezzrow/settings/local_luke.py
@@ -4,8 +4,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mezzrow',
-        'USER': 'luke',
-        'PASSWORD': '',
+        'USER': 'lukeaune',
+        'PASSWORD': 'mezzrow',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'ATOMIC_REQUESTS': True,
