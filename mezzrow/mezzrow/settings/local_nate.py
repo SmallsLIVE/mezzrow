a48c598dde5511c069b5ce18b5cebc4f6c50f0e5
Nate Aune <nate@appsembler.com>
2014-07-08 21:28:41 -0400

    added debug toolbar

--- mezzrow/mezzrow/settings/local_nate.py
+++ mezzrow/mezzrow/settings/local_nate.py
@@ -12,5 +12,9 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN")
