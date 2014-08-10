1d20b2d3755a24c26e08a3bdaef498bba87fa5ce
Filip Jukic <filip@jukic.me>
2014-08-10 23:41:52 +0200

    renamed set_time to set (yes, again)

--- mezzrow/oscar_apps/catalogue/models.py
+++ mezzrow/oscar_apps/catalogue/models.py
@@ -4,9 +4,9 @@ from oscar.apps.catalogue.abstract_models import AbstractProduct
 
 class Product(AbstractProduct):
     event = models.ForeignKey('events.Event', related_name='products', null=True)
-    set_time = models.CharField(max_length=50, blank=True)
+    set = models.CharField(max_length=50, blank=True)
 
     class Meta(AbstractProduct.Meta):
-        ordering = ['id', 'set_time', 'title']
+        ordering = ['id', 'set', 'title']
 
 from oscar.apps.catalogue.models import *
