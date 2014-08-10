a90727057cf80ea1c38fd3290db684aaf433ba13
Filip Jukic <filip@jukic.me>
2014-08-10 20:35:40 +0200

    multiple slots/sets for an event

--- mezzrow/oscar_apps/catalogue/models.py
+++ mezzrow/oscar_apps/catalogue/models.py
@@ -4,5 +4,6 @@ from oscar.apps.catalogue.abstract_models import AbstractProduct
 
 class Product(AbstractProduct):
     event = models.ForeignKey('events.Event', related_name='products', null=True)
+    slot = models.CharField(max_length=50, blank=True)
 
 from oscar.apps.catalogue.models import *
