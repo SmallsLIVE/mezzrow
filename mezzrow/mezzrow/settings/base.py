b4095baa4ffcbb1ccd26a02332a78e0972fc2415
Filip Jukic <filip@jukic.me>
2014-08-26 12:37:51 +0200

    disable messages on the site

--- mezzrow/mezzrow/settings/base.py
+++ mezzrow/mezzrow/settings/base.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.humanize',
-    'django.contrib.messages',
+    #'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
