d27808214f8cf8ae6b81a3893df3fef7e639acdf
Filip Jukic <filip@jukic.me>
2014-09-02 10:07:36 +0200

    small changes to tickets on the event detail screen

--- mezzrow/oscar_apps/partner/availability.py
+++ mezzrow/oscar_apps/partner/availability.py
@@ -5,5 +5,5 @@ class MezzrowPolicy(CoreStockRequired):
     @property
     def message(self):
         if self.num_available > 0:
-            return "{0} seats available".format(self.num_available)
+            return "{0} guaranteed seats available".format(self.num_available)
         return "Sold out - join wait list"
