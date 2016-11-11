89ef006a249237a9bb8fe6b34811f3dd5df6fbe6
Jim Sam <jamessam@users.noreply.github.com>
2016-11-10 21:06:09 -0800

    unit tests for the checkout forms.

--- mezzrow/site_app/tests.py
+++ mezzrow/site_app/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from oscar_apps.checkout.forms import GatewayForm
 
-# Create your tests here.
+class TestForms(TestCase):
+
+    def test_checkout_good_data(self):
+        data = {
+            'username' : 'test@user.com',
+            'first_name' : 'Test',
+            'last_name' : 'User',
+            'options' : 'anonymous',
+        }
+        form = GatewayForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_checkout_bad_email(self):
+        data = {
+            'username' : 'test@user.con',
+            'first_name' : 'Test',
+            'last_name' : 'User',
+            'options' : 'anonymous',
+        }
+        form = GatewayForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_checkout_bad_first(self):
+        data = {
+            'username' : 'test@user.com',
+            'first_name' : 'TestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTestedTested',
+            'last_name' : 'User',
+            'options' : 'anonymous',
+        }
+        form = GatewayForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_checkout_bad_last(self):
+        data = {
+            'username' : 'test@user.com',
+            'first_name' : 'Test',
+            'last_name' : 'UsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsersUsers',
+            'options' : 'anonymous',
+        }
+        form = GatewayForm(data=data)
+        self.assertFalse(form.is_valid())
